b00588dff49f68e50c3c0408c03e38dea9e8490c
SlickYeet <lasse@famlam.ca>
2025-06-21 09:32:19 -0700

    fix: update import statement to remove file extension

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import type { NextConfig } from "next"
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-import "./src/env.js"
+import "./src/env"
 
 const nextConfig: NextConfig = {
   /* config options here */
