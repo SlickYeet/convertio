a412506baea0775ad7d8e7cb2657a24efb1e542a
SlickYeet <lasse@famlam.ca>
2025-06-21 12:51:20 -0700

    fix: correct margin top value in Credits component for improved layout

--- src/components/credits.tsx
+++ src/components/credits.tsx
@@ -2,7 +2,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 export function Credits() {
   return (
-    <div className="text-muted-foreground mt-12 flex items-center justify-center text-center text-sm lg:mt-18">
+    <div className="text-muted-foreground mt-12 flex items-center justify-center text-center text-sm lg:mt-15">
       <p className="mr-1.5">Made with ❤️ by</p>
       <a
         href="https://lasse.famlam.ca"
