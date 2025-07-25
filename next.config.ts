7f885a3541680e51461500da69f7ee118f3a01fd
SlickYeet <lasse@famlam.ca>
2025-07-24 20:18:48 -0700

    chore: remove environment validation import from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from "next"
 
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-import "./src/env"
-
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
