2f08b7cd67e39d136c1a9dd8572f578302ac73c6
SlickYeet <lasse@famlam.ca>
2026-01-31 18:58:27 -0800

    refactor: rename project from Convertio to Cnvrtr

--- src/components/landing/cta.tsx
+++ src/components/landing/cta.tsx
@@ -9,7 +9,7 @@ export function CTA() {
           We are open source and free to use!
         </h2>
         <p className="text-primary-foreground mx-auto mb-8 max-w-2xl text-center text-xl text-balance opacity-90">
-          Convertio is an open-source project, help us by staring it on GitHub
+          Cnvrtr is an open-source project, help us by staring it on GitHub
           and contributing to its development. Your support means a lot to us!
         </p>
 
--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
-  appName: "Convertio",
+  appName: "Cnvrtr",
   description: "Easily convert Markdown files to PDF.",
-  githubUrl: "https://github.com/SlickYeet/convertio",
+  githubUrl: "https://github.com/lx2dev/cnvrtr",
   isDev: process.env.NODE_ENV === "development",
 }
